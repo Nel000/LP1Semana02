7b6c0141e06a02b1eb3db8799b67c134b9a37e30
Manuel Geraldes <45165444+Nel000@users.noreply.github.com>
2022-03-02 14:53:01 +0000

    Add OpInt project

---
+++ OpInt/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace OpInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i1 = 3;
+            int i2 = 1;
+            int i3 = -8;
+            int x = 2;
+            
+            // 1)
+            // x = i1 - i3;
+            // 3 - 1 = 2
+
+            // 2)
+            // x = i3 / i1;
+            // -8 / 3 = -2.66666... = -2
+
+            // 3)
+            // x += i2;
+            // x = 2 + 1 = 3
+
+            // 4)
+            // x *= i1;
+            // x = 2 * 3 = 6
+
+            // 5)
+            x %= i2 - 0b1001;
+            // x = 2 % 1 - 9 = 
+            
+            Console.WriteLine(x);
+        }
+    }
+}
