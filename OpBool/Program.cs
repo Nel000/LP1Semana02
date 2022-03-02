5814faf282f0c4b9588f8041aa1f945b48204ff5
Manuel Geraldes <45165444+Nel000@users.noreply.github.com>
2022-03-02 21:52:55 +0000

    Add more incomplete examples to OpInt and OpBool programs

--- OpBool/Program.cs
+++ OpBool/Program.cs
@@ -14,23 +14,39 @@ namespace OpBool
 
             // 1)
             // x = true && false;
-            // x = false;
+            // x = false
 
             // 2)
             // x = !true || false;
-            // x = false;
+            // x = false
 
             // 3)
             // x = true ^b2;
-            // x = true;
+            // x = true
 
             // 4)
             // x &= f2 < f1;
-            // x = true;
+            // x = true
 
             // 5)
             // x ^= !(f2 != f1);
             // x = true
+
+            // 6)
+            // x |= !true ^ b1;
+            // x = 
+
+            // 7)
+            x = b1 && b2 && !(f1 >= f2);
+            // x = 
+
+            // 8)
+            // x &= b1 ^ b2 ^ (f1 == f2);
+            // x = 
+
+            // 9)
+            // x ^= !(b1 || b2);
+            // x = 
             
             Console.WriteLine(x);
         }
