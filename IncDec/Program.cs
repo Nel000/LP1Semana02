8f3c7f6fe9791c7b7a56c196704edcd2ff2aecf3
Manuel Geraldes <45165444+Nel000@users.noreply.github.com>
2022-03-02 14:30:49 +0000

    Show variable value after operations before and after suffix

---
+++ IncDec/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace IncDec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5, b = 2, c, d;
+
+            // Increments 'a' and 'b', only updates after operation
+            // a = 5 + 1; b = 2 + 1; c = 5 + 2
+            c = a++ + b++;
+            Console.WriteLine(c);
+
+            // Decrements 'a' and 'b' before operation 
+            // a = 6 - 1; b = 3 - 1; c = 5 + 2
+            d = --a + --b;
+            Console.WriteLine(d);
+        }
+    }
+}
