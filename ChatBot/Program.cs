699f9abacc4ca1631d432921d3e639402e20be3e
Manuel Geraldes <45165444+Nel000@users.noreply.github.com>
2022-03-03 15:55:19 +0000

    Add user questions

---
+++ ChatBot/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ChatBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Determines when the program should end
+            bool exit;
+
+            // Stores user question and possible program answers
+            string question, answer;
+
+            // Run cycle while exit is false
+            do
+            {
+                switch (question)
+                {
+                    case "EXIT":
+                        exit = true;
+                        break;
+                    case "What is your name?":
+
+                        break;
+                    case "What are you?":
+
+                        break;
+                    case "What is your purpose?":
+
+                        break;
+                    case "How will you achieve your goal?":
+
+                        break;
+                }
+            } while (!exit);
+        }
+    }
+}
