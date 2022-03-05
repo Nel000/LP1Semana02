9e5d5261891ac1bd85b69e8b632cd0c590c20f56
Manuel Geraldes <manuelgeraldes2009@hotmail.com>
2022-03-05 23:28:23 +0000

    Add main program loop

---
+++ Piramide/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Piramide
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Bool for exit condition
+            bool exit = false;
+
+            // Variables to store number of lines
+            int lines;
+            string sLines;
+
+            // Introductory message
+            Console.WriteLine("Insert a number of lines for a triangle");
+            
+            // Run program while exit conditions are not met
+            do
+            {
+                // Get input from user and convert to INT
+                sLines = Console.ReadLine();
+                lines = int.Parse(sLines);
+            
+                // Execute rest of program if lines are equal or bigger than 2
+                if (lines >= 2)
+                {
+                    
+                }
+                else
+                {
+                    // Request a number bigger than 1
+                    Console.WriteLine("Please insert a number bigger that 1");
+                }
+            } while (!exit);
+            
+        }
+    }
+}
